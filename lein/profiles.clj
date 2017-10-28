0e53046890243c79dfa143d568f0405771f93447
Johannes Staffans <johannes.staffans@gmail.com>
2017-10-28 21:57:26 +0200

    Update profiles

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,25 +1,16 @@
 {:user  {:plugins  [[com.jakemccrary/lein-test-refresh  "0.10.0"]
-                    [lein-pprint  "1.1.1"]
-                    [lein-clojars "0.9.1"]
-                    [lein-localrepo "0.5.3" :exclusions [org.clojure/clojure]]]
-         :dependencies  [[reloaded.repl "0.2.1"]
+                    [lein-pprint  "1.1.1"]]
+         :dependencies  [[reloaded.repl "0.2.4"]
                          [org.clojure/tools.nrepl "0.2.12"]
                          [org.clojure/tools.namespace "0.2.11"]
-                         [refactor-nrepl "2.2.0"]
-                         [cider/cider-nrepl  "0.12.0"]
+                         [refactor-nrepl "2.3.1"]
+                         [cider/cider-nrepl  "0.14.0"]
                          [pjstadig/humane-test-output "0.7.0"]
-                         [spyscope "0.1.5"]
-                         [alembic  "0.3.2"]
-                         [im.chit/vinyasa.inject "0.3.4"]
-                         [im.chit/vinyasa.reflection "0.3.4"]]
-         :injections
-         [(require 'spyscope.core)
-          (require 'pjstadig.humane-test-output)
-          (pjstadig.humane-test-output/activate!)
-          (require '[vinyasa.inject :as inject])
-
-          (inject/in clojure.core
-                     [clojure.java.shell sh]
-                     [alembic.still  [distill pull] lein  [load-project pull-project]]
-                     [clojure.tools.namespace.repl refresh]
-                     [vinyasa.reflection .& .> .? .* .% .%>])]}}
+                         [spyscope "0.1.5"]]
+         ;; :injections
+         ;; TODO: spyscope does not seem to work with clj 1.9
+         ;; [(require 'spyscope.core)
+         ;;  (require 'pjstadig.humane-test-output)
+         ;;  (pjstadig.humane-test-output/activate!)]
+         :deploy-repositories [["clojars" {:url "https://clojars.org/repo"
+                                           :sign-releases false}]]}}
